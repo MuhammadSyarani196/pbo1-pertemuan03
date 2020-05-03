1298f8cad7129a1aed3ee95cc3c789e3f182ee9e
MuhammadSyarani196 <ahmadsyarani51@gmail.com>
2020-05-03 23:46:05 +0700

    commmit isi bebas

---
+++ src/percabangan/Switch.java
@@ -0,0 +1,24 @@
+package percabangan;
+
+public class Switch {
+    public static void main(String[] args) {
+        int jalanJalan=1;
+        switch(jalanJalan){
+            case 1:
+                System.out.println("Urang bungas");
+                break;
+            case 2:
+                System.out.println("Urang Gagah");
+                break;
+            case 3:
+                System.out.println("Urang Cantik");
+                break;
+            case 4:
+                System.out.println("Kada bungas");
+                break;
+            default:
+                System.out.println("mahluk halus");
+                break;
+        }
+    }
+}
